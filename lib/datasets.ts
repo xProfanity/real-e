08ef7c8dc5b49ed0b15e8e878579d5f90e32972d
xprofanity <pecpalmplay@gmail.com>
2025-04-14 02:14:31 +0200

    made arrays of notable areas around blantyre urban residential areas

---
+++ lib/datasets.ts
@@ -0,0 +1,178 @@
+const blantyreUrbanAreas = [
+    "Blantyre City Center",
+    "Limbe",
+    "Ndirande",
+    "Chilomoni",
+    "Zingwangwa",
+    "Bangwe",
+    "Machinjiri",
+    "Chirimba",
+    "Mbayani",
+    "Chiwembe",
+    "Soche",
+    "Kameza",
+    "Chichiri",
+    "Nancholi",
+    "Chigumula",
+    "Lunzu",
+    "Kabula",
+    "Sunny Side",
+    "Naperi",
+    "New Naperi"
+];
+
+const limbeSubAreas = [
+    "Limbe Central",
+    "South Lunzu",
+    "Nkolokoti",
+    "Makheta",
+    "Kachere",
+    "Mapanga",
+    "Chichiri",
+    "Maselema",
+    "Mudi Estate",
+    "Maone",
+    "Blantyre Teachers College",
+    "Angelo Goveya",
+    "Chiradzulu",
+    "Chinyonga",
+    "Kanjedza",
+    "Nguludi",
+    "Bangwe",
+    "Namatapa",
+    "Mpingwe",
+    "Manje",
+    "Misesa",
+    "Chiwembe",
+    "Namiyango",
+    "Chikunda",
+    "Chigumula",
+    "Bvumbe",
+    "BCA",
+    "St. Patricks (Mzedi)"
+  ];
+  
+  const ndirandeSubAreas = [
+    "Makata",
+    "Chinseu",
+    "Golio",
+    "Safalao",
+    "Newlines",
+    "Ndirande Ringroad",
+    "Kachere",
+    "Nkolokoti",
+    "Naotcha"
+  ];
+
+  const chilomoniSubAreas = [
+    "Chilomoni Central",
+    "Beehive Area",
+    "Mount Michiru Foothills",
+    "Chilomoni Market Area",
+    "Chilomoni Township"
+  ];
+
+  const bangweSubAreas = [
+    "Lilaka",
+    "Mvula",
+    "Chikunda",
+    "Namiyango",
+    "Ntopwa 2",
+    "Nthandizi",
+    "Banana",
+    "Bisiyere"
+  ];
+
+  const machinjiriSubAreas = [
+    "Area 8",
+    "Area 9",
+    "Area 10",
+    "Chinupule",
+    "Chinupule 1A",
+    "Chinupule 1C",
+    "Chinupule 2",
+    "Gulani",
+    "Khama",
+    "Manjombe",
+    "Nkung'Unda"
+  ];
+
+  const chirimbaSubAreas = [
+    "Chirimba Industrial Area",
+    "Chirimba Township",
+    "Chirimba Market Area",
+    "Chirimba Health Centre Area",
+    "Chirimba Primary School Area"
+  ];
+
+  const mbayaniSubAreas = [
+    "Mbayani Market Area",
+    "Mbayani Primary School Area",
+    "Zalewa Roadside",
+    "Mbayani Hill Area",
+    "Mbayani Health Centre Area",
+    "Old Mbayani",
+    "New Mbayani"
+  ];
+
+  const chiwembeLocations = [
+    "Chiwembe Township",
+    "Chiwembe Market",
+    "Chiwembe Health Centre",
+    "Chiwembe Stadium",
+    "Chiwembe Dam"
+  ];
+
+  const socheSubAreas = [
+    "Soche East",
+    "Soche West",
+    "Soche Central",
+    "Soche Hill",
+    "Soche Market Area",
+    "Soche Health Centre Area",
+    "Soche Primary School Area"
+  ];
+
+  const kamezaSubAreas = [
+    "Kameza Roundabout",
+    "Kameza Industrial Area",
+    "Kameza Market",
+    "Kameza Primary School Area",
+    "Kameza Residential Area",
+    "Kameza Health Centre"
+  ];
+
+  const nancholiSubAreas = [
+    "Nancholi Chiimire",
+    "Nancholi Health Centre",
+    "Nancholi Youth Organization",
+    "Nancholi Market",
+    "Nancholi River Area"
+  ];
+
+  const chigumulaLocations = [
+    "Chigumula Market",
+    "Chigumula Forest Reserve",
+    "Chigumula Primary School",
+    "Chigumula Residential Area",
+    "Chigumula Health Centre"
+  ];
+
+  const lunzuLocations = [
+    "Lunzu Market",
+    "Lunzu Township",
+    "Lunzu Hill",
+    "Lunzu Health Centre",
+    "Lunzu Police Unit",
+    "Lunzu Residential Area"
+  ];
+
+  const kabulaLocations = [
+    "Kabula Hill",
+    "Kabula Market",
+    "Kabula Residential Area",
+    "Kabula Health Centre",
+    "Kabula Police Unit",
+    "Kabula Lodge"
+  ];
+  
